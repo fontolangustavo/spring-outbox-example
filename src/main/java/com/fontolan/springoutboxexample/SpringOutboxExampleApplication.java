d45d5aa45064d4c71fb378614e826435a506551a
Fontolan <fontolangustavo@gmail.com>
2024-06-19 12:38:30 -0300

    chore: init repo

---
+++ src/main/java/com/fontolan/springoutboxexample/SpringOutboxExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fontolan.springoutboxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOutboxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOutboxExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fontolan/springoutboxexample/SpringOutboxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontolan.springoutboxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOutboxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
