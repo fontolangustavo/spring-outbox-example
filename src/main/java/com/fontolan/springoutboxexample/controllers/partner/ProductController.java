c919274e9665c40e3e57a4c5bd14417dedbb2fa2
Fontolan <fontolangustavo@gmail.com>
2024-07-18 11:18:42 -0300

    feat: ensure update product with queue

--- src/main/java/com/fontolan/springoutboxexample/controllers/partner/ProductController.java
+++ src/main/java/com/fontolan/springoutboxexample/controllers/partner/ProductController.java
@@ -6,6 +6,7 @@ import com.fontolan.springoutboxexample.controllers.partner.request.ProductReque
 import com.fontolan.springoutboxexample.controllers.partner.response.ProductResponse;
 import com.fontolan.springoutboxexample.models.Product;
 import com.fontolan.springoutboxexample.usecases.CreateProductUseCase;
+import com.fontolan.springoutboxexample.usecases.UpdateProductUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController("partner-products")
 public class ProductController {
     private final CreateProductUseCase createProductUseCase;
+    private final UpdateProductUseCase updateProductUseCase;
     @Qualifier("partner.product-mapper")
     private final ProductMapper productMapper;
 
-    public ProductController(CreateProductUseCase createProductUseCase, ProductMapper productMapper) {
+    public ProductController(
+            CreateProductUseCase createProductUseCase,
+            UpdateProductUseCase updateProductUseCase,
+            ProductMapper productMapper
+    ) {
         this.createProductUseCase = createProductUseCase;
+        this.updateProductUseCase = updateProductUseCase;
         this.productMapper = productMapper;
     }
 
@@ -29,5 +36,19 @@ public class ProductController {
         Product product = createProductUseCase.store(productMapper.toProduct(request));
 
         return ResponseEntity.ok().body(productMapper.toProductResponse(product));
+    }
+
+    @PutMapping("/{externalId}")
+    public ResponseEntity<Void> update(
+            @PathVariable String externalId,
+            @Valid @ModelAttribute ProductRequest request
+    ) throws JsonProcessingException {
+        Product product = productMapper.toProduct(request);
+
+        product.setExternalId(externalId);
+
+        updateProductUseCase.update(product);
+
+        return ResponseEntity.noContent().build();
     }
 }
